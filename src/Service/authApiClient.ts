fc521ceda5ea7586bbb9b3aed0afd206df89d2d5
zishan344 <zishanahmed344@gmail.com>
2025-04-21 17:00:54 +0600

    Refactor authApiClient to use default export and improve structure

--- src/Service/authApiClient.ts
+++ src/Service/authApiClient.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
+const liveUrl = "https://hemolyze.vercel.app/";
+const authApiClient = axios.create({
+  baseURL: `${liveUrl}api/v1/`,
+});
 
-export const authApiClient = axios.interceptors.request.use(
+export default authApiClient;
+
+authApiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access_token");
     if (token) {
