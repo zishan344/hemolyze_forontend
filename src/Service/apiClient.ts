033960206a99a222d8d76af409e75124f61ba9e5
zishan344 <zishanahmed344@gmail.com>
2025-04-28 13:20:42 +0600

    feat: Update API client base URL to use live server for production

--- src/Service/apiClient.ts
+++ src/Service/apiClient.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const liveUrl = "https://hemolyze.vercel.app/";
-const localUrl = "http://127.0.0.1:8000/";
+const liveUrl = "https://hemolyze.vercel.app/";
+// const localUrl = "http://127.0.0.1:8000/";
 const apiClient = axios.create({
-  baseURL: `${localUrl}api/v1/`,
+  baseURL: `${liveUrl}api/v1/`,
 });
 
 export default apiClient;
